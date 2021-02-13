a70e5c279667a180f7cd23ec88fae656259046b7
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-02-13 12:05:50 +0100

    Add ugly-but-working implementation of Paapi5.request

--- lib/paapi5.ex
+++ lib/paapi5.ex
@@ -3,16 +3,54 @@ defmodule Paapi5 do
   Documentation for `Paapi5`.
   """
 
-  @doc """
-  Hello world.
+  @service "ProductAdvertisingAPI"
 
-  ## Examples
+  defmodule Request do
+    defstruct [:method, :url, :body, :headers]
+  end
 
-      iex> Paapi5.hello()
-      :world
+  def request(access_key, secret_key, partner_tag, _marketplace, operation, payload) do
+    host = "webservices.amazon.it"
+    region = "eu-west-1"
 
-  """
-  def hello do
-    :world
+    endpoint = "https://#{host}/paapi5/searchitems"
+    headers = %{
+      # "host" => @host,
+      # "content-type" => "application/json; charset=UTF-8",
+      "x-amz-target" => "com.amazon.paapi5.v1.ProductAdvertisingAPIv1.SearchItems",
+      "content-encoding" => "amz-1.0",
+    }
+
+    payload = %{
+      "PartnerTag" => partner_tag,
+      "PartnerType" => "Associates",
+      "Marketplace" => "www.amazon.it",
+      "Operation" => operation
+    } |> Map.merge(payload)
+
+    current_time = DateTime.utc_now() |> DateTime.to_naive()
+    encoded_payload = Jason.encode!(payload)
+
+    signed_header =
+      Paapi5.Auth.sign(
+        access_key,
+        secret_key,
+        "POST",
+        endpoint,
+        region,
+        @service,
+        encoded_payload,
+        headers,
+        current_time
+      )
+
+    headers = [{"content-type", "application/json; charset=UTF-8"} | signed_header]
+
+    %Request{
+      method: "POST",
+      url: endpoint,
+      body: encoded_payload,
+      headers: headers
+    }
   end
 end
--- test/paapi5_test.exs
+++ test/paapi5_test.exs
@@ -1,8 +1,31 @@
 defmodule Paapi5Test do
   use ExUnit.Case
-  doctest Paapi5
 
-  test "greets the world" do
-    assert Paapi5.hello() == :world
+  @access_key System.get_env("ACCESS_KEY")
+  @secret_key System.get_env("SECRET_KEY")
+  @partner_tag System.get_env("PARTNER_TAG")
+
+  test "builds valid request to perform operations on Amazon Product Advertising Api" do
+    request =
+      Paapi5.request(
+        @access_key,
+        @secret_key,
+        @partner_tag,
+        :it,
+        "SearchItems",
+        %{"Keywords" => "das kapital"}
+      )
+
+    response = HTTPoison.request!(request.method, request.url, request.body, request.headers)
+
+    assert response.status_code == 200
+    assert %{
+             "SearchResult" => %{
+               "Items" => [
+                 %{"ASIN" => _asin, "DetailPageUrl" => "https://www.amazon." <> _rest_url}
+                 | _rest_items
+               ]
+             }
+           } = response.body |> Jason.decode!()
   end
 end
