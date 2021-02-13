6d2ae4026370fa3dd7d4ab19de1fb13d982aebf1
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-02-13 11:19:04 +0100

    Add request signing
    Code to sign requests is imported from the aws_auth package.
    Some parts have been removed or simplified.

---
+++ lib/paapi5/auth.ex
@@ -0,0 +1,133 @@
+defmodule Paapi5.Auth do
+  @moduledoc false
+
+  def sign(
+        access_key,
+        secret_key,
+        http_method,
+        url,
+        region,
+        service,
+        payload,
+        headers,
+        request_time
+      ) do
+    uri = URI.parse(url)
+
+    params = URI.decode_query(uri.query || "")
+
+    headers = Map.put_new(headers, "host", uri.host)
+
+    payload = hash_sha256(payload)
+
+    headers = Map.put_new(headers, "x-amz-content-sha256", payload)
+
+    amz_date = request_time |> format_time()
+    date = request_time |> format_date()
+
+    headers = Map.put_new(headers, "x-amz-date", amz_date)
+
+    scope = "#{date}/#{region}/#{service}/aws4_request"
+
+    string_to_sign =
+      build_canonical_request(
+        http_method,
+        uri.path || "/",
+        params,
+        headers,
+        payload
+      )
+      |> build_string_to_sign(amz_date, scope)
+
+    signature =
+      build_signing_key(secret_key, date, region, service)
+      |> build_signature(string_to_sign)
+
+    signed_headers =
+      Enum.map(headers, fn {key, _} -> String.downcase(key) end)
+      |> Enum.sort(&(&1 < &2))
+      |> Enum.join(";")
+
+    auth_header =
+      "AWS4-HMAC-SHA256 Credential=#{access_key}/#{scope},SignedHeaders=#{signed_headers},Signature=#{
+        signature
+      }"
+
+    headers
+    |> Map.put("authorization", auth_header)
+    |> Map.to_list()
+  end
+
+  defp build_canonical_request(http_method, path, params, headers, hashed_payload) do
+    query_params = URI.encode_query(params) |> String.replace("+", "%20")
+
+    header_params =
+      Enum.map(headers, fn {key, value} -> "#{String.downcase(key)}:#{String.trim(value)}" end)
+      |> Enum.sort(&(&1 < &2))
+      |> Enum.join("\n")
+
+    signed_header_params =
+      Enum.map(headers, fn {key, _} -> String.downcase(key) end)
+      |> Enum.sort(&(&1 < &2))
+      |> Enum.join(";")
+
+    encoded_path =
+      path
+      |> String.split("/")
+      |> Enum.map(fn segment -> URI.encode_www_form(segment) end)
+      |> Enum.join("/")
+
+    "#{http_method}\n#{encoded_path}\n#{query_params}\n#{header_params}\n\n#{signed_header_params}\n#{
+      hashed_payload
+    }"
+  end
+
+  defp build_string_to_sign(canonical_request, timestamp, scope) do
+    hashed_canonical_request = hash_sha256(canonical_request)
+    "AWS4-HMAC-SHA256\n#{timestamp}\n#{scope}\n#{hashed_canonical_request}"
+  end
+
+  defp build_signing_key(secret_key, date, region, service) do
+    hmac_sha256("AWS4#{secret_key}", date)
+    |> hmac_sha256(region)
+    |> hmac_sha256(service)
+    |> hmac_sha256("aws4_request")
+  end
+
+  defp build_signature(signing_key, string_to_sign) do
+    hmac_sha256(signing_key, string_to_sign)
+    |> bytes_to_string
+  end
+
+  def hash_sha256(data) do
+    :crypto.hash(:sha256, data)
+    |> bytes_to_string
+  end
+
+  def hmac_sha256(key, data) do
+    :crypto.hmac(:sha256, key, data)
+  end
+
+  def bytes_to_string(bytes) do
+    Base.encode16(bytes, case: :lower)
+  end
+
+  defp format_time(time) do
+    formatted_time =
+      time
+      |> NaiveDateTime.to_iso8601()
+      |> String.split(".")
+      |> List.first()
+      |> String.replace("-", "")
+      |> String.replace(":", "")
+
+    formatted_time <> "Z"
+  end
+
+  defp format_date(date) do
+    date
+    |> NaiveDateTime.to_date()
+    |> Date.to_iso8601()
+    |> String.replace("-", "")
+  end
+end
---
+++ test/paapi5/auth_test.exs
@@ -0,0 +1,36 @@
+defmodule Paapi5.AuthTest do
+  use ExUnit.Case, async: true
+  @time ~N[2013-05-24 01:23:45]
+
+  test "sign request correctly" do
+    headers =
+      Map.new()
+      |> Map.put("Date", "Fri, 24 May 2013 00:00:00 GMT")
+      |> Map.put("x-amz-target", "com.amazon.paapi5.v1.ProductAdvertisingAPIv1.SearchItems")
+      |> Map.put("content-encoding", "amz-1.0")
+
+    signed_request =
+      Paapi5.Auth.sign(
+        "AKIAIOSFODNN7EXAMPLE",
+        "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+        "POST",
+        "https://webservices.amazon.it/paapi5/searchitems",
+        "eu-west-1",
+        "ProductAdvertisingAPI",
+        "{\"some\": \"payload\"}",
+        headers,
+        @time
+      )
+
+    {"authorization", "AWS4-HMAC-SHA256 " <> request_parts} =
+      signed_request |> List.keyfind("authorization", 0)
+
+    request_parts = String.split(request_parts, ",") |> Enum.map(&String.split(&1, "="))
+
+    assert request_parts == [
+             ["Credential", "AKIAIOSFODNN7EXAMPLE/20130524/eu-west-1/ProductAdvertisingAPI/aws4_request"],
+             ["SignedHeaders", "content-encoding;date;host;x-amz-content-sha256;x-amz-date;x-amz-target"],
+             ["Signature", "0d498722be0f482804e18f323971fc128e8f2430d64fbc4651a2085ebfce743e"]
+           ]
+  end
+end
