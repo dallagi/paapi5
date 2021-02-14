82e47042a8ff07fa0a1371cb83365edfb805c4d6
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-02-14 12:09:33 +0100

    Support elixir 1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Paapi5.MixProject do
     [
       app: :paapi5,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
