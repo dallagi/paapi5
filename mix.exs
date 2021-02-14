16d262ed7a26b8ac2de060f992c955f57a81b7af
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-02-14 11:52:22 +0100

    Add credo

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule Paapi5.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:jason, "~> 1.2"},
       {:httpoison, "~> 1.8", only: [:dev, :test]}
